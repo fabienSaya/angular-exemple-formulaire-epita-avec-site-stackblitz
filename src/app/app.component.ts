b43fa335f64add8f4512f3e09d3d9888fb7e904b
fabienSaya <fsayatravail@hotmail.com>
2022-04-22 11:27:18 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, VERSION } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
@@ -11,14 +11,17 @@ export class AppComponent {
 
   constructor() {
     this.userForm = new FormGroup({
-      firstname: new FormControl(''),
+      firstname: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+      ]),
       lastname: new FormControl(''),
-      email: new FormControl(''),
+      email: new FormControl('', Validators.email),
       address: new FormGroup({
         street: new FormControl(''),
         postalCode: new FormControl(''),
-        city: new FormControl('')
-      })
+        city: new FormControl(''),
+      }),
     });
 
     console.log(this.userForm);
@@ -26,6 +29,9 @@ export class AppComponent {
 
   onSubmitForm() {
     console.log(this.userForm.value);
+    //check s'il est valide
+    console.log(this.userForm.valid);
+    console.log(this.userForm);
   }
 
   setForm() {
